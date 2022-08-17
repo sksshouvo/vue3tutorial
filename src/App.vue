666225133cc58f61a36a752ddf8f9aef55f233ff
Salman Kabir <sksshouvo2@gmail.com>
2022-08-17 08:02:17 +0600

    All The Text Binding Functions are described here

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Textbinding/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Textbinding from './components/Textbinding.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Textbinding
   }
 }
 </script>
---
+++ src/components/Textbinding.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>{{greet}} {{name}}</div>    
+    <div v-text="testText"></div>
+    <div v-html="renderHtml"></div>
+    <h2 v-bind:id="headingId">Test Heading</h2>
+    <button type="button" v-bind:disabled="isDisabled">Disabled Button</button>
+</template>
+<script>
+export default {
+    name: "App",
+    data () {
+        return {
+            greet: "Hello",
+            name: "Salman Kabir",
+            testText: "<b>Hi There</b>",
+            renderHtml: `<a href='#' onClick="alert('Hello There')">Click me to get Alert</a>`,
+            headingId: "heading",
+            isDisabled: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>

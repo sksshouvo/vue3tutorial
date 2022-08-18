cedd7cb720612105aba2cfe6302f7483b001f9c3
Salman Kabir <sksshouvo2@gmail.com>
2022-08-18 08:28:32 +0600

    Learning vue 3 text binding attributes

--- src/components/Textbinding.vue
+++ src/components/Textbinding.vue
@@ -4,6 +4,10 @@
     <div v-html="renderHtml"></div>
     <h2 v-bind:id="headingId">Test Heading</h2>
     <button type="button" v-bind:disabled="isDisabled">Disabled Button</button>
+    <h2 class="underline">Underline Text</h2>
+    <h2 class="underline" v-bind:class="status">Status</h2>
+    <h1 v-bind:class="isPromoted && 'promoted'">Promoted Movie</h1>
+    <h2 v-bind:class="soldOut ? 'soldout' : 'new'">Sold-Out? Movie</h2>
 </template>
 <script>
 export default {
@@ -15,12 +19,26 @@ export default {
             testText: "<b>Hi There</b>",
             renderHtml: `<a href='#' onClick="alert('Hello There')">Click me to get Alert</a>`,
             headingId: "heading",
-            isDisabled: true
+            isDisabled: true,
+            status: "danger",
+            isPromoted: true,
+            soldOut: false
         }
     }
 }
 </script>
 
 <style scoped>
-
+    .underline{
+        text-decoration: underline;
+    }
+    .promoted {
+        font-style: italic;
+    }
+    .soldout {
+        color: red;
+    }
+    .new {
+        color: green;
+    }
 </style>
